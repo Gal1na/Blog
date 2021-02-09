7662bc3710171d0a18250aaa977af7ec3d2d1524
Galina-lucky <gala.nitch@gmail.com>
2021-02-09 14:13:38 +0200

    Fixed controls

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,20 @@
 
     menuLink.on('click', function() {
       menu.removeClass('menu--show');
-    });    
+    }); 
+
+    let articles = document.querySelectorAll('.article-preview');
+    let filter = document.querySelector('.custom-select__list');
+
+    filter.onchange = function() {
+      for (let article of articles) {
+        if ((article.dataset.category !== filter.value) && (filter.value !== 'all')) {
+          article.parentNode.classList.add('hidden');
+        } else {
+          article.parentNode.classList.remove('hidden');
+        }
+      }
+    }
   });
 
   $(window).on('scroll', function() {
